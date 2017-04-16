f0a3df7cfb03cdd955c182af805d7466340c4cbd
Abhishek Gupta <abhishekgupt34@gmail.com>
2017-04-16 12:38:46 +0530

    More Code

--- mst.c
+++ mst.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <limits.h>
 
 
 #ifdef DEBUG
@@ -19,6 +20,27 @@ int nextIdle(int *busy, int n, int* idle){
     }
     return -1;
 }
+//Following function is taken from stackoverflow -- http://stackoverflow.com/questions/5901476/sending-and-receiving-2d-array-over-mpi
+int **alloc_2d_int(int rows, int cols) {
+    int *data = (int *)malloc(rows*cols*sizeof(int));
+    int **array= (int **)malloc(rows*sizeof(int*));
+    for (int i=0; i<rows; i++)
+        array[i] = &(data[cols*i]);
+
+    return array;
+}
+
+//partial solution
+//n elem array - contains points to the next element in the cycle
+//currCost occured
+//bestCost up to date
+//cur elem which has to take decision
+//flag - which tells if this is the subtree which reported to master the current bestCost
+//
+
+//two new tags - 
+//UPDATE BESTCOST_TAG, to inform master of the best cost
+//BEST SOL - to inform of the best solution
 
 int main(int argc, char *argv[]){
     int i;
@@ -48,28 +70,55 @@ int main(int argc, char *argv[]){
     
     if (myrank == 0){ //master code
         //Receive Input
+        int n,i,j;
+        printf("Enter number of nodes\n");
+        scanf("%d",&n);
+        int **graph = alloc_2d_int(n,n);
+        for (i=0;<n;i++)
+            graph[i] = (int*)malloc(n*sizeof(int));
+        printf("Enter adjacency matrix\n");
+        for (i=0;i<n;i++)
+            for (j=0;j<n;j++)
+                scanf("%d", &graph[i][j]);
+        DBG("MASTER : Input Received\n");
+        //Send data to all other processors
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&graph[0][0], n*n, MPI_INT, 0, MPI_COM_WORLD);
+        DBG("MASTER : Data Broadcast successfull!\n");
         int idle = nProc - 1;
         tag_t tag;
         int *busy = (int*)malloc(nProc*sizeof(int));
         for (i=0;i<nProc;i++) busy[i] = 0;
         busy[0] = 1;
         int dst = nextIdle(busy, nProc, &idle);
-        int total_weight = 0, bestSolVal = 0;
-        for (i=0;i<n && total_weight<=bag_size;i++){
-            total_weight += inp[i].weight;
-            bestSolVal += inp[i].value;
-            bestSol[i] = 1;
-        }
-        if (total_weight > bag_size){ bestSol[i-1] = 0; bestSolVal -= inp[i-1].value;}
-        bestSol[n] = bestSolVal;
-        tempSol[n] = bestSolVal;
-        MPI_Send(tempSol, n+1, MPI_INT, dst, PBM_TAG, MPI_COMM_WORLD);
+        int bestCost = MAX_INT;
+        //find Initial Solution ----------------------- TODO
+        int *initial_sP = (int*)malloc((n+4)*sizeof(int));
+        for (i=0;i<n;i++)
+            initial_sP[i] = 0;
+        initial_sP[n] = 0; //curCost
+        initial_sP[n+1] = bestCost; //bestCost
+        initial_sP[n+2] = 0; //elem which has to take decision
+        initial_sP[n+3] = 0;  //flag to check if to to dfs
+
+        //Send initial problem to firstIdle processor
+        int *bestSol = (int*)malloc((n+4)*sizeof(int));
+        int *tempSol = (int*)malloc((n+4)*sizeof(int));
+        int src;
+        MPI_Send(initial_sP, n+4, MPI_INT, dst, PBM_TAG, MPI_COMM_WORLD);
+        MPI_Status status;
         while (idle != nProc-1){
-            MPI_Status status;
             MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             tag = status.MPI_TAG;
-            int src = status.MPI_SOURCE;
-            if (tag == SOLVE_TAG) { //receive best solution value and best solution
+            src = status.MPI_SOURCE;
+            if (tag == SOLVE_TAG) { 
+                //receive best solution value and best solution
+                MPI_Recv(tempSol, n+4, MPI_INT, src, tag, MPI_COMM_WORLD, &status);
+                if (tempSol[n+1] < bestCost){
+                    bestCost = tempSol[n+1];
+                    for (i=0;i<n;i++)
+                        bestSol[i] = tempSol[i];
+                }
             }
             if (tag == IDLE_TAG) { //processor is idle
                 MPI_Recv(&tag, 1, MPI_INT, src, tag, MPI_COMM_WORLD,  &status);
@@ -104,50 +153,72 @@ int main(int argc, char *argv[]){
         MPI_Finalize();
     }
     else {  //slave code
-        list_t list;
+        int n;
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        int ** graph = alloc_2d_int(n,n);
+        MPI_Bcast(&graph[0][0], n*n, MPI_INT, 0, MPI_COM_WORLD);
+        list_t list; //stack for storing problems
         list.head = NULL; list.len = 0;
-        int bestSolVal;
-        //axSp is same as axSol
+        int bestCost, currCost, flag;
+        MPI_Status status;
+        int src;
         while (1){
             MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             tag_t tag = status.MPI_TAG;
-            int src = status.MPI_SOURCE;
+            src = status.MPI_SOURCE;
             if (tag == END_TAG){
-                DBG("Processor %d : Game over!\n", myrank);
+                DBG("Processor %d: Program over!\n", myrank);
                 MPI_Recv(&tag, 1, MPI_INT, src, tag, MPI_COMM_WORLD, &status);
                 break;
             }
             if (tag == PBM_TAG) {
-                DBG("I am %d, %d asked me to work\n", myrank, src);
+                DBG("Processor %d: %d asked me to work\n", myrank, src);
+                int *auxSp = (int*)malloc((n+4)*sizeof(int)):
                 //Receive subproblem
-                //Update bestSolVal
+                MPI_Recv(auxSp, n+4, MPI_INT, src, tag, MPI_COMM_WORLD,&status);
+                //Update flag, bestCost and currCost
+                flag = auxSp[n+3];
+                bestCost = auxSp[n+1];
+                currCost = auxSp[n];
                 //Insert the problem into list
+                insert_into_list(&list, auxSp, n+4);
                 while (!empty_list(&list)){
                     int *auxSp = remove_from_list(&list);
-                    int high = upper_bound(auxSp);
-                    DBG("Upper bound calculated by %d = %d\n",myrank, high);
-                    if (high > bestSolVal){
-                        int low = lower_bound(auxSp, n);
-                        DBG("Lower bound calculated by %d = %d\n",myrank, low);
-                        if (low > bestSolVal)  {
-                            //Update bestSolVal here
+                    int low = lower_bound(auxSp,n);
+                    DBG("Lower bound calculated by %d = %d\n",myrank, high);
+                    if (low < bestCost){
+                        int high = upper_bound(auxSp, n);
+                        DBG("Upper bound calculated by %d = %d\n",myrank, high);
+                        if (high < bestCost)  {
+                            //Update bestCost here
+                            bestCost = high;
+                            auxSp[n+1] = high;
                             //Send bestSolVal and solution to the master
+                            MPI_Send(auxSp, n+4, MPI_INT, 0, SOLVE_TAG, MPI_COMM_WORLD);
                         }
                         if (low != high){ 
                             int data[2];
-                            data[0] = high;
+                            data[0] = low;
                             data[1] = list.len;
                             MPI_Send(data, 2, MPI_INT, 0, BnB_TAG, MPI_COMM_WORLD);
                             int *assigned = (int*) malloc((list.len+1)*sizeof(int));
+                            int total;
                             //Recive bestSolVal and processors assigned
-                            //assign received bestSolVal
+                            MPI_Recv(assigned, list.len+1, MPI_INT, 0, BnB_TAG, MPI_COMM_WORLD, &status);
                             if (status.MPI_TAG == BnB_TAG){
-                                int total; //number of processor assigned
+                                MPI_Get_count(&status, MPI_INT, &total);
+                                total--;
+                                //assign received bestSolVal
+                                bestCost = assigned[total];
+                                auxSp[n+1] = bestCost;
                                 //branch the problem
+                                branch(&list, auxSp);
+                                int *sp;
                                 for (i=0;i<total;i++){
                                     //remove from list
+                                    sp = remove_from_list(&list):
                                     //send bestSolVal and removed subproblem to the assigned node
-                                    MPI_Send(sp, n+1, MPI_INT, assigned[i], PBM_TAG, MPI_COMM_WORLD);
+                                    MPI_Send(sp, n+4, MPI_INT, assigned[i], PBM_TAG, MPI_COMM_WORLD);
                                 }
                             }
                             else{
